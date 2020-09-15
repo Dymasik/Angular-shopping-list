550de9559cf47715027e77e20c52bb7797379c4e
Dima Kuntso <st.dymond.stq@gmail.com>
2020-09-15 19:05:36 +0300

    Databinding enhancment

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -10,9 +10,14 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is test', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg')
   ];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @Output() onItemAdded = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput', {static: false}) nameInput: ElementRef<HTMLInputElement>;
+  @ViewChild('amountInput', {static: false}) amountInput: ElementRef<HTMLInputElement>;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddRecipe() {
+    this.onItemAdded.emit(new Ingredient(this.nameInput.nativeElement.value, parseInt(this.amountInput.nativeElement.value)));
   }
 
 }
